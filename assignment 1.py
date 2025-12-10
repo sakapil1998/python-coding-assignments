533278db3f2bc2cc7a8fd37b84dc1d877295725b
sakapil1998 <kkmggc@gmail.com>
2025-12-11 04:58:38 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,25 @@
+###Task 1:
+
+"""
+basic mathmatic operations like addition, subtraction, multiplication, division:
+
+"""
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+
+p =a+b
+q =a-b
+r =a*b 
+s =a/b
+
+print("p is",p)
+print("q is",q)
+print("r is",r)
+print("s is",s)
+
+###Task 2:
+
+a=input("enter first name:")
+b=input("enter second name:")
+
+print("hello", a,b+"!","Welcome to the Python programe.")
