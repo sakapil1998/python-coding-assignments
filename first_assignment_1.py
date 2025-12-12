fb1908aac18f5f6a5f4f66e51346ae3dcb6c8780
sakapil1998 <kkmggc@gmail.com>
2025-12-12 18:49:51 +0530

    Update first_assignment_1.py

--- first_assignment_1.py
+++ first_assignment_1.py
@@ -7,15 +7,15 @@ basic mathmatic operations like addition, subtraction, multiplication, division:
 a=float(input("Enter first number:"))
 b=float(input("Enter second number:"))
 
-addition =a+b
-subtraction =a-b
-multiplication =a*b 
-division =a/b
+add =a+b
+sub =a-b
+multi =a*b 
+div =a/b
 
-print("addtion is",addition)
-print("subtraction is",subtraction)
-print("multiplication is",multiplication)
-print("addition is",division)
+print("addtion is",add)
+print("subtraction is",sub)
+print("multiplication is",multi)
+print("addition is",div)
 
 ###Task 2:
 
@@ -23,3 +23,4 @@ a=input("enter first name:")
 b=input("enter second name:")
 
 print("hello", a,b+"!","Welcome to the Python programe.")
+
