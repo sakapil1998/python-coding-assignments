dbeaa1dad90972324585a4ee81330118bdfa3e24
sakapil1998 <kkmggc@gmail.com>
2025-12-12 20:41:41 +0530

    initial commit

---
+++ 1st_assignment_1.py
@@ -0,0 +1,37 @@
+###Task 1:
+
+"""
+program to perform basic arithmetic operations
+
+"""
+a=5
+b=10
+add: int =a+b
+sub: int =a-b
+multi: int =a*b
+div: float =a/b
+print("addition is",add)
+print("subtraction is",sub)
+print("multiplication is", multi)
+print("division is", div)
+
+
+"""Expected output:"""
+    #addtion is 15
+    #subtraction is -5
+    #multiplication is 50
+    #addition is 0.5"
+
+###Task 2:
+"""
+program to print a welcome message with first name and last name
+
+"""
+
+first_name="john"
+last_name="doe"
+
+print("hello", first_name, last_name+"!","Welcome to the Python programe.")
+
+"""Expected output:"""
+     #hello john doe! Welcome to the Python programe.
