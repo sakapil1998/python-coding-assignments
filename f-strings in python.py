36996ee9ff5514769e4def9858601bdb376142ac
sakapil1998 <kkmggc@gmail.com>
2025-12-12 19:12:45 +0530

    initial commit

---
+++ f-strings in python.py
@@ -0,0 +1,17 @@
+##using f-strings
+name="kapil"
+age=27
+language="Python"
+hours=3
+print(f"{name} is {age} years old. He studies {language} {hours} hours a day.")
+
+###
+a=45
+b=100
+c=10
+print(f"i scored in {a+b+c} marks in exam")
+
+a=input("enter first name:")
+b=input("enter second name:")
+
+print(f"hello, {a} {b}!, Welcome to the Python programe.")
