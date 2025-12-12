59fe935c0e90ea00540bf3759a5f6bd14d467277
sakapil1998 <kkmggc@gmail.com>
2025-12-12 19:22:43 +0530

    initial commit

---
+++ slycing in string.py
@@ -0,0 +1,5 @@
+###Index of slicing: string[index]
+#Syntax of slicing: string[start:end:step]
+#start: starting index at which the slicing operation starts
+#end: ending index at which the slicing stops(excluded)
+#step: integer that specifies the step for the slicing
