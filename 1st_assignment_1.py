1d37715893736eee80ce2bd3eeb177fca83ca9a2
sakapil1998 <kkmggc@gmail.com>
2025-12-16 05:56:59 +0530

    initial commit

--- 1st_assignment_1.py
+++ 1st_assignment_1.py
@@ -4,12 +4,12 @@
 program to perform basic arithmetic operations
 
 """
-a=5
-b=10
-add: int =a+b
-sub: int =a-b
-multi: int =a*b
-div: float =a/b
+a= int(input("Enter first number(a):"))
+b=int(input("Enter second number(b):"))
+add = int(a+b)
+sub = int(a-b)
+multi = int(a*b)
+div = float(a/b)
 print("addition is",add)
 print("subtraction is",sub)
 print("multiplication is", multi)
@@ -17,10 +17,13 @@ print("division is", div)
 
 
 """Expected output:"""
-    #addtion is 15
+
+    #Enter first number(a):5
+    #Enter second number(b):10
+    #addition is 15
     #subtraction is -5
     #multiplication is 50
-    #addition is 0.5"
+    #division is 0.5
 
 ###Task 2:
 """
@@ -28,10 +31,12 @@ program to print a welcome message with first name and last name
 
 """
 
-first_name="john"
-last_name="doe"
+first_name = input("Enter first_name:")
+last_name = input("Enter last_name:")
 
 print("hello", first_name, last_name+"!","Welcome to the Python programe.")
 
 """Expected output:"""
+     #Enter first_name:john 
+     #Enter last_name:doe
      #hello john doe! Welcome to the Python programe.
